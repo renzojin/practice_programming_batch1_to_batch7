d1bdc0686d4db621d79570d818d5bb91a85eaca5
renzojin <jeanneagustin09@gmail.com>
2026-03-19 18:14:40 +0800

    sum of all ten numbers program has been added.

---
+++ program7_sum_of_all_ten_numbers.py
@@ -0,0 +1,12 @@
+from colorama import Fore, Style, init
+init(autoreset=True)
+
+
+#Prog07: Sum of 10 numbers
+total = 0
+for i in range(10):
+    n = int(input(Fore.YELLOW + "Enter number: "))
+    total += n
+
+print(total)
+print(" ")
