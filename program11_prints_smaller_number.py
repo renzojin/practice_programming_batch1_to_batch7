d4ff392ef717660e1cee2f7b1019659e52304576
renzojin <jeanneagustin09@gmail.com>
2026-04-02 22:30:53 +0800

    prints smaller number program has been added.

---
+++ program11_prints_smaller_number.py
@@ -0,0 +1,13 @@
+from colorama import Fore, Style, init
+init(autoreset=True)
+
+#Program: Print the smaller number
+num1 = int(input(Fore.CYAN + "Enter First Number: "))
+num2 = int(input(Fore.CYAN + "Enter Second Number: "))
+
+if num1 < num2:
+    print(num1)
+elif num2 < num1:
+    print(num2)
+else:
+    print("Equal")
